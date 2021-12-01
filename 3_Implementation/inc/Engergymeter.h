e47ea0979ba0aa186b867a83b7110c8bebc93628
Ajish Saji <80235704+legends07@users.noreply.github.com>
2021-12-02 00:33:12 +0530

    Update Engergymeter.h

--- 3_Implementation/inc/Engergymeter.h
+++ 3_Implementation/inc/Engergymeter.h
@@ -1,1 +1,6 @@
+#define enable            1
+#define registerselection 0
 
+void send_a_command(unsigned char command);
+void send_a_character(unsigned char character);
+void send_a_string(char *string_of_characters);
