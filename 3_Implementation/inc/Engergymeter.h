2634d7634bd48a34d2e7baf7d8d07b627ef99b52
Ajish Saji <80235704+legends07@users.noreply.github.com>
2021-12-02 13:22:21 +0530

    Update Engergymeter.h

--- 3_Implementation/inc/Engergymeter.h
+++ 3_Implementation/inc/Engergymeter.h
@@ -1,5 +1,20 @@
-#define enable            1
-#define registerselection 0
+/**
+ * @file Engergymeter.h
+ * @author Ajish Saji (ajish.saji@ltts.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <avr/io.h>
+#define F_CPU 1000000
+#include <util/delay.h>
+#include <stdlib.h>
+
+#define enable            5
+#define registerselection 6
 
 void send_a_command(unsigned char command);
 void send_a_character(unsigned char character);
